31d913da190bf7bd0cba6362e9a26c5911554848
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2025-02-21 17:40:54 +0500

    Variables, Data types, Operators, Types.
    Variables, Data types, Operators, Types.

---
+++ Input.py
@@ -0,0 +1,37 @@
+# Input (input result for input() always string) ager ham numer bhe dyain gy tab bhe string consider hoga
+
+input("enter your name:")
+
+name = input("enter your name:")
+print("Welcome", name)
+
+age = input("enter your age:")
+print("You entered", age)
+
+val = input("enter some value:")
+print(type(val), val)
+
+int("6")
+val = int(input("enter some value:"))
+print(type(val), val)
+
+float("8")
+val = float(input("enter some value:"))
+print(type(val), val)
+
+
+name = input("enter name:")
+age = input("enter age:")
+marks = input("enter marks:")
+
+print("Welcome", name)
+print("age", age)
+print("marks:", marks)
+
+name = input("enter name:")
+age = int(input("enter age:"))
+marks = float(input("enter marks:"))
+
+print("Welcome", name)
+print("age", age)
+print("marks:", marks)
---
+++ Relational.Comparison.py
@@ -0,0 +1,11 @@
+# Relational/Comparison Operators 
+# Answer (True/False)
+a = 80
+b = 30
+
+print(a == b) # a == b mean equal
+print(a != b) # a != b mean notequal
+print(a >= b) # >= greaterthan equal to
+print(a > b) # greaterthan
+print(a <= b) # <= lessthan equal to
+print(a < b) # lessthan
